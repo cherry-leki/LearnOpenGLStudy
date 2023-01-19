4d68961f146d44154a49e696771d0a83ff77e8d0
CL <chaelin.kim@kaist.ac.kr>
2023-01-19 22:47:08 +0900

    Add utils file
    window size callback 함수, key input 처리 함수를 utils 파일에 정리

---
+++ python/01_getting_started/utils.py
@@ -0,0 +1,13 @@
+import glfw
+from OpenGL.GL import *
+
+### functions
+# whenever the window size changed (by OS or user) this callback function executes
+def framebuffer_size_callback(window, width, height):
+    glViewport(0, 0, width, height)
+
+# process all input
+# : query GLFW whether relevant keys are pressed/released this frame and react accordingly
+def process_input(window):
+    if(glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.PRESS):
+        glfw.set_window_should_close(window, True)
